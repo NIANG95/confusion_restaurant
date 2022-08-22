f448c537a63486d4e4b08fa6bf5e157cda530fce
Mame Sandeck Niang <niangmamesandeck@gmail.com>
2022-08-22 17:20:08 +0000

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,16 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
